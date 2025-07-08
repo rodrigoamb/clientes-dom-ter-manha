9c5edd20d6febd0a6500f1744dd60e79f77a3d0a
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-07-08 11:53:09 -0300

    create

---
+++ script.js
@@ -0,0 +1,80 @@
+const form = document.getElementById("form-cliente");
+const tabela = document.getElementById("tabela-clientes");
+const modalEditar = document.getElementById("modal-editar");
+const modalExcluir = document.getElementById("modal-excluir");
+
+// ---- inputs -----
+
+const inputNome = document.getElementById("nome");
+const inputSobrenome = document.getElementById("sobrenome");
+const inputcpf = document.getElementById("cpf");
+const inputEmail = document.getElementById("email");
+
+// ----- inputs do modal editar -----
+
+const inputEditNome = document.getElementById("edit-nome");
+const inputEditSobrenome = document.getElementById("edit-sobrenome");
+const inputEditCpf = document.getElementById("edit-cpf");
+const inputEditEmail = document.getElementById("edit-email ");
+
+const formEdicao = document.getElementById("form-edicao");
+const btnCancelarEdicao = document.getElementById("cancelar-edicao");
+const btnConfirmarExclusao = document.getElementById("confirmar-exclusao");
+const btnCancelarExclusao = document.getElementById("cancelar-exclusao");
+
+let clientes = [
+  {
+    nome: "Rodrigo",
+    sobrenome: "Medeiros",
+    cpf: "122334567",
+    email: "rodrigo@mail.com",
+  },
+  {
+    nome: "Rodrigo",
+    sobrenome: "Medeiros",
+    cpf: "122334567",
+    email: "rodrigo@mail.com",
+  },
+  {
+    nome: "Rodrigo",
+    sobrenome: "Medeiros",
+    cpf: "122334567",
+    email: "rodrigo@mail.com",
+  },
+];
+
+let indexEditando = null;
+let indexExcluindo = null;
+
+function renderizarTabela() {
+  clientes.forEach((cliente) => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+        <td>${cliente.nome}</td>
+        <td>${cliente.sobrenome}</td>
+        <td>${cliente.cpf}</td>
+        <td>${cliente.email}</td>
+    `;
+
+    const tdAcoes = document.createElement("td");
+    tdAcoes.classList.add("acoes");
+
+    const btnEditar = document.createElement("button");
+    btnEditar.textContent = "Editar";
+    btnEditar.classList.add("editar");
+
+    const btnExcluir = document.createElement("button");
+    btnExcluir.textContent = "Excluir";
+    btnExcluir.classList.add("excluir");
+
+    tdAcoes.appendChild(btnEditar);
+    tdAcoes.appendChild(btnExcluir);
+
+    tr.appendChild(tdAcoes);
+
+    tabela.appendChild(tr);
+  });
+}
+
+renderizarTabela();
